1cfc90734b70a6edbc56b8381d1967a91e217dbe
Thais <2025106239@unibrasil.com>
2025-08-28 17:58:18 -0300

    - Correction of individual evaluation variables  - Insertion of calculations of monthly and annual indexes of good and excellent evaluations

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+// fazer a mesma coisa com as avaliações ruins/pessimas ::)))
+
 
 import java.util.Scanner;
 public class Main {
@@ -17,6 +19,13 @@ public class Main {
         int sumJuly = 0;
         int contador = 0;
         int nextSesion;
+        int sumExcellentGoodJanuary =0;
+        int sumExcellentGoodFebruary = 0;
+        int sumExcellentGoodMarch = 0;
+        int sumExcellentGoodApril = 0;
+        int sumExcellentGoodMay = 0;
+        int sumExcellentGoodJune = 0;
+        int sumExcellentGoodJuly = 0;
         boolean b = false;
 
         do {
@@ -47,8 +56,10 @@ public class Main {
                     System.out.println("Type number of bads reviews: ");
                     int badJanuary = sc.nextInt();
                     System.out.println("Type number of terrible reviews: ");
-                    terrible = sc.nextInt();
+                    int terribleJanuary = sc.nextInt();
                     sc.nextLine();
+                    sumExcellentGoodJanuary = excellentJanuary+goodJanuary;
+                    int sumBadTerribleJanuary = badJanuary+terribleJanuary;
                     sumJanuary = excellentJanuary + goodJanuary + neutralJanuary + bad + terrible;
                     System.out.println("The total of number surveys answered this month was: " + sumJanuary);
                     System.out.println("If you want to next session for calculate yours indicators type 1 ");
@@ -58,100 +69,110 @@ public class Main {
                 case 2:
                     System.out.println("===FEBRUARY===");
                     System.out.println("Type number of excellent reviews: ");
-                    excellent = sc.nextInt();
+                    int excellentFebruary = sc.nextInt();
                     System.out.println("Type number of goods reviews: ");
-                    good = sc.nextInt();
+                    int goodFebruary = sc.nextInt();
                     System.out.println("Type number of neutrals reviews: ");
-                    neutral = sc.nextInt();
+                    int neutralFebruary = sc.nextInt();
                     System.out.println("Type number of bads reviews: ");
-                    bad = sc.nextInt();
+                    int badFebruary = sc.nextInt();
                     System.out.println("Type number of terrible reviews: ");
-                    terrible = sc.nextInt();
+                    int terribleFebruary = sc.nextInt();
                     sc.nextLine();
 
+                    int sumBadTerribleFebruary = badFebruary+terribleFebruary;
+                    sumExcellentGoodFebruary = excellentFebruary+goodFebruary;
                     sumFebruary = excellent + good + neutral + bad + terrible;
                     System.out.println("The total of number surveys answered this month was: " + sumFebruary);
                     break;
                 case 3:
                     System.out.println("===MARCH===");
                     System.out.println("Type number of excellent reviews: ");
-                    excellent = sc.nextInt();
+                    int excellentMarch = sc.nextInt();
                     System.out.println("Type number of goods reviews: ");
-                    good = sc.nextInt();
+                    int goodMarch = sc.nextInt();
                     System.out.println("Type number of neutrals reviews: ");
-                    neutral = sc.nextInt();
+                    int neutralMarch = sc.nextInt();
                     System.out.println("Type number of bads reviews: ");
-                    bad = sc.nextInt();
+                    int badMarch = sc.nextInt();
                     System.out.println("Type number of terrible reviews: ");
-                    terrible = sc.nextInt();
+                    int terribleMarch = sc.nextInt();
 
-
-                    sumMarch = excellent + good + neutral + bad + terrible;
+                    int sumBadTerribleMarch = badMarch+terribleMarch;
+                    sumExcellentGoodMarch = excellentMarch+goodMarch;
+                    sumMarch = excellentMarch + goodMarch + neutralMarch + badMarch + terribleMarch;
                     System.out.println("The total of number surveys answered this month was: " + sumMarch);
                     break;
                 case 4:
                     System.out.println("===APRIL===");
                     System.out.println("Type number of excellent reviews: ");
-                    excellent = sc.nextInt();
+                    int excellentApril = sc.nextInt();
                     System.out.println("Type number of goods reviews: ");
-                    good = sc.nextInt();
+                    int goodApril = sc.nextInt();
                     System.out.println("Type number of neutrals reviews: ");
-                    neutral = sc.nextInt();
+                    int neutralApril = sc.nextInt();
                     System.out.println("Type number of bads reviews: ");
-                    bad = sc.nextInt();
+                    int badApril= sc.nextInt();
                     System.out.println("Type number of terrible reviews: ");
-                    terrible = sc.nextInt();
+                    int terribleApril = sc.nextInt();
 
-
-                    sumApril = excellent + good + neutral + bad + terrible;
+                    int sumBadTerribleApril = badApril+terribleApril;
+                    sumExcellentGoodApril = excellentApril+goodApril;
+                    sumApril = excellentApril + goodApril + neutralApril + badApril + terribleApril;
                     System.out.println("The total of number surveys answered this month was: " + sumApril);
                     break;
                 case 5:
                     System.out.println("===MAY===");
                     System.out.println("Type number of excellent reviews: ");
-                    excellent = sc.nextInt();
+                    int excellentMay = sc.nextInt();
                     System.out.println("Type number of goods reviews: ");
-                    good = sc.nextInt();
+                    int goodMay = sc.nextInt();
                     System.out.println("Type number of neutrals reviews: ");
-                    neutral = sc.nextInt();
+                    int neutralMay = sc.nextInt();
                     System.out.println("Type number of bads reviews: ");
-                    bad = sc.nextInt();
+                    int badMay = sc.nextInt();
                     System.out.println("Type number of terrible reviews: ");
-                    terrible = sc.nextInt();
+                    int terribleMay = sc.nextInt();
 
-                    sumMay = excellent + good + neutral + bad + terrible;
+                    int sumBadTerribleMay = badMay+terribleMay;
+                    sumExcellentGoodMay = excellentMay+goodMay;
+                    sumMay = excellentMay + goodMay + neutralMay + badMay + terribleMay;
                     System.out.println("The total of number surveys answered this month was: " + sumMay);
                     break;
                 case 6:
-                    System.out.println("===JULY===");
+                    System.out.println("===JUNE===");
                     System.out.println("Type number of excellent reviews: ");
-                    excellent = sc.nextInt();
+                    int excellentJune = sc.nextInt();
                     System.out.println("Type number of goods reviews: ");
-                    good = sc.nextInt();
+                    int goodJune = sc.nextInt();
                     System.out.println("Type number of neutrals reviews: ");
-                    neutral = sc.nextInt();
+                   int neutralJune = sc.nextInt();
                     System.out.println("Type number of bads reviews: ");
-                    bad = sc.nextInt();
+                    int badJune = sc.nextInt();
                     System.out.println("Type number of terrible reviews: ");
-                    terrible = sc.nextInt();
+                    int terribleJune = sc.nextInt();
 
-                    sumJune = excellent + good + neutral + bad + terrible;
+                    int sumBadTerrobleJune= badJune+terribleJune;
+                    sumExcellentGoodJune = excellentJune+goodJune;
+                    sumJune = excellentJune + goodJune + neutralJune + badJune + terribleJune;
                     System.out.println("The total of number surveys answered this month was: " + sumJune);
                     break;
                 case 7:
                     System.out.println("===JULY===");
                     System.out.println("Type number of excellent reviews: ");
-                    excellent = sc.nextInt();
+                    int excellentJuly = sc.nextInt();
                     System.out.println("Type number of goods reviews: ");
-                    good = sc.nextInt();
+                    int goodJuly = sc.nextInt();
                     System.out.println("Type number of neutrals reviews: ");
-                    neutral = sc.nextInt();
+                    int neutralJuly = sc.nextInt();
                     System.out.println("Type number of bads reviews: ");
-                    bad = sc.nextInt();
+                    int badJuly = sc.nextInt();
                     System.out.println("Type number of terrible reviews: ");
-                    terrible = sc.nextInt();
+                    int terribleJuly = sc.nextInt();
 
-                    sumJuly = excellent + good + neutral + bad + terrible;
+
+                    sumExcellentGoodJuly = excellentJuly + goodJuly + neutralJuly + badJuly + terribleJuly;
+                    sumJuly = excellentJuly + goodJuly + neutralJuly + badJuly + terribleJuly;
                     System.out.println("The total of number surveys answered in this month was: " + sumJuly);
                     break;
                 case 8:
@@ -161,8 +182,12 @@ public class Main {
                     double mediaAnnual = totalYear/6;
                     double percentageChange = ((sumJuly - mediaAnnual) / mediaAnnual) * 100;
                     System.out.println("Monthly variation was: "+percentageChange+"%");
-                    int mediaTotalBoasRuins;
-
+                    int somaTotalBoasOtimasAnual = sumExcellentGoodJanuary+sumExcellentGoodFebruary+sumExcellentGoodMarch+sumExcellentGoodApril+sumExcellentGoodMay+sumExcellentGoodJune+sumExcellentGoodJuly;
+                    double mediaBoasOtimasAnual = somaTotalBoasOtimasAnual/7;
+                    double variacaoAnualBoasOtimas = ((sumExcellentGoodJuly - mediaBoasOtimasAnual)/mediaBoasOtimasAnual)*100;
+                    double variacaoMensalBoasOtimas = ((sumExcellentGoodJune + sumExcellentGoodJuly)/sumExcellentGoodJune)*100;
+                    System.out.println("The annual variation good and excellent was:"+variacaoAnualBoasOtimas);
+                    System.out.println("The monthly variation good and excllent was: "+variacaoMensalBoasOtimas);
                     break;
                 default:
                     System.out.println("Invalid option! Try again");
