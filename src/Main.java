bc7b4969197088525d84ef3ab7b77baf8e197aa5
Thais <2025106239@unibrasil.com>
2025-09-18 18:18:19 -0300

    -Implementação total de array - Implementação cálculo da equipe - Inicío formatação da mensagem de percentual para apenas duas casas decimais

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 
-import java.sql.SQLOutput;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -36,9 +36,10 @@ public class Main {
             otimasBoas[i] = otima + boa;
             pessimasRuins[i] = pessima+ruim;
             totalMensal[i] = otima+boa+neutra+ruim+pessima;
-            System.out.println("|================================================================================|");
-            System.out.println("|A quantidade total de avaliações no mês: "+nomeMeses[i]+"foi: "+totalMensal[i]+"|");
-            System.out.println("|================================================================================|");
+            System.out.println("|==========================|");
+            System.out.println("|Total do mês de " +nomeMeses[i]+ ": " +totalMensal[i]);
+            System.out.println("|==========================|\n");
+
 
         }
         int totalAnual = 0;
@@ -60,17 +61,17 @@ public class Main {
 
         System.out.println("\n === RESULTADOS===\n");
 
-        System.out.println("INDICADORES ANUAIS\n");
+        System.out.println("|---INDICADORES ANUAIS---|\n");
         double variacaoTotalAnual = ((totalMensal[mesAtual] - mediaAnual) / mediaAnual) * 100;
-        System.out.println("A variação anual foi de: "+variacaoTotalAnual+"%\n");
+        System.out.println(String.format("A variação anual total foi de: %.2f%%", variacaoTotalAnual));
 
         double variacaoAnualOtimasBoas = ((otimasBoas[mesAtual] - mediaOtimasBoas)/mediaOtimasBoas)*100;
-        System.out.println("A variação anual de avaliações ótimas e boas foi de: "+variacaoAnualOtimasBoas+"%\n");
+        System.out.println(String.format("A variação anual de avaliações ótimas e boas foi de: %.2f%%"+variacaoAnualOtimasBoas+"\n"));
 
         double variacaoAnualOPessimasRuins = ((pessimasRuins[mesAtual] - mediaPessimasRuins)/mediaPessimasRuins)*100;
-        System.out.println("A variação anual de avaliações péssimas e ruins foi de: "+ variacaoAnualOPessimasRuins +"%\n");
+        System.out.println(String.format("A variação anual de avaliações péssimas e ruins foi de: %.2f%%"+ variacaoAnualOPessimasRuins +"%\n"));
 
-        System.out.println("INDICADORES MENSAIS\n");
+        System.out.println("|---INDICADORES MENSAIS---|\n");
         double variacaoMensalTotal = ((double) ( totalMensal[mesAtual] - totalMensal[mesAnterior]) / totalMensal[mesAnterior]) * 100;
         System.out.println("A variação mensal no total de avalicações foi de: "+variacaoMensalTotal+"%\n");
 
@@ -80,30 +81,62 @@ public class Main {
         double variacaoMensalPessimasRuins = ((double) ( pessimasRuins[mesAtual] - pessimasRuins[mesAnterior]) / pessimasRuins[mesAnterior]) * 100;
         System.out.println("A variação mensal de avaliações péssimas e ruins foi de: "+variacaoMensalOtimasBoas+"%\n");
 
-        System.out.println("====CALCULOS INDIVIDUAIS===");
+
         Scanner sc2 = new Scanner(System.in);
-        int [] chamadosResolvidos = new int [100];
-        int [] chamadosCertificados = new int[100];
+        int [] chamadosResolvidos = new int [1];
+        int [] chamadosCertificados = new int[1];
         String[] nomes = new String[100];
-        double percentual [] = new double[100];
-        int resolvidos = 0;
-        int certificados = 0;
+        double percentual [] = new double[1];
+        int resolvidosIndividual = 0;
+        int certificadosIndividual = 0;
         String analista;
 
         for (int i = 0; i < chamadosCertificados.length; i++){
-            System.out.println("===CHAMADOS CERTIFICADOS===");
+            System.out.println("====CALCULOS PERCENTUAIS INDIVIDUAIS===\n");
             System.out.println("ANALISTA: ");
+            System.out.println(" ");
             analista = sc2.next();
             System.out.println("TOTAL RESOLVIDOS: ");
-            resolvidos = sc2.nextInt();
+            System.out.println(" ");
+            resolvidosIndividual = sc2.nextInt();
             System.out.println("TOTAL CERTIFICADOS: ");
-            certificados = sc2.nextInt();
+            System.out.println(" ");
+            certificadosIndividual = sc2.nextInt();
+            System.out.println(" ");
 
-            chamadosResolvidos[i] = resolvidos;
-            chamadosCertificados[i] = certificados;
 
-            percentual [i] = (double)certificados / resolvidos;
+            chamadosResolvidos[i] = resolvidosIndividual;
+            chamadosCertificados[i] = certificadosIndividual;
+
+            percentual [i] = (double) certificadosIndividual / resolvidosIndividual;
             System.out.println("A porcentagem é: "+percentual[i]);
+
+        }
+        String [] equipe = {"Suporte - Gerencial", "Suporte - Integração" , "Suporte - Plataforma" , "Suporte - SAP"};
+        int [] resolvidosEquipe = new int[4];
+        int [] certificadosEquipe = new int [4];
+        double [] percentualEquipe =  new double[4];
+        int resolvidosEquipeVar = 0;
+        int certificadosEquipeVar = 0;
+
+         for (int i = 0; i < equipe.length ; i++) {
+             System.out.println("====CALCULOS PERCENTUAIS EQUIPE===\n");
+
+             System.out.println("EQUIPE: "+equipe[i]);
+             System.out.println("TOTAL RESOLVIDOS: ");
+             System.out.println(" ");
+             resolvidosEquipeVar = sc2.nextInt();
+             System.out.println("TOTAL CERTIFICADOS: ");
+             System.out.println(" ");
+             certificadosEquipeVar = sc2.nextInt();
+
+             resolvidosEquipe[i] = resolvidosEquipeVar;
+             certificadosEquipe[i] = certificadosEquipeVar;
+
+             percentualEquipe [i] = (double) certificadosEquipeVar / resolvidosEquipeVar;
+             System.out.println("A porcentagem é: "+percentualEquipe[i]);
+
+
 
         }
 
